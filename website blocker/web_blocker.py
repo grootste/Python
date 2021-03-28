c9e1e39d22df62e01c246286d84ae85582c9931a
grootste <46301514+grootste@users.noreply.github.com>
2021-03-28 14:09:50 +0530

    Add files via upload

---
+++ website blocker/web_blocker.py
@@ -0,0 +1,30 @@
+import time
+from datetime import datetime as dt
+
+host_temp="hosts"
+hosts_path="C:\Windows\System32\drivers\etc\hosts"
+redirect="127.0.0.1"
+website_list=["wwww.facebook.com", "facebook.com", "singh.baibhav321@gmail.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,16) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,17):
+        print("woking hours")
+        with open(host_temp,'r+') as file:
+            content=file.read()
+            print(content)
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write("\n"+redirect+" "+ website+"\n")
+    else:
+        with open(host_temp,'r+') as file:
+            content=file.readline()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+                    time.sleep(0.005)
+                file.truncate()  
+        print("Fun Hours")
+    time.sleep(1)
